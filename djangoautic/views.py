ccb7cf213fd60ac40339beedafd1492c3a46d56c
eman-mohamed <eman.mohamed@fresh.com.eg>
2022-07-18 09:32:02 +0200

    first commit

---
+++ djangoautic/views.py
@@ -0,0 +1,11 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+
+
+def homePage(request):
+    #return HttpResponse('home')
+    return render(request,'homepage.html')
+
+def about(request):
+#    return HttpResponse('about')
+    return render(request,'about.html')
