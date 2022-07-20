b09da36c1352644a411b82a859a3c3c79ec4b4ed
eman-mohamed <eman.mohamed@fresh.com.eg>
2022-07-20 23:11:19 +0200

    admin & template tags & model methods and Static Files & Images

--- djangoautic/urls.py
+++ djangoautic/urls.py
@@ -17,9 +17,14 @@ from django.contrib import admin
 from django.urls import path
 from django.conf.urls import url,include
 from . import views
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     url(r'^articles/',include('articles.urls')),
     url(r'^about/$',views.about),
     url(r'^$',views.homePage)
 ]
+
+urlpatterns += staticfiles_urlpatterns()
