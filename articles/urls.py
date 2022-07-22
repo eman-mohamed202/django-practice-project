1c8c1747192a35853fdb0cf6e505fff601b5480d
eman-mohamed <eman.mohamed@fresh.com.eg>
2022-07-23 00:48:45 +0200

    extending tepmlates & url parametars & named urls & article detail template

--- articles/urls.py
+++ articles/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
 from django.conf.urls import url
 from . import views
+
+app_name = 'articles'
 urlpatterns = [
-    url(r'^$',views.article_list)
+    url(r'^$',views.article_list,name="list"),
+    url(r'^(?P<slug>[\w-]+)/$',views.article_detail,name="detail")
 ]
